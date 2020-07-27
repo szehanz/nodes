85a7d1d3923025e7ada68a8109a09fcf1ea42c80
ozorob2 <omar.zorob@yahoo.com>
2020-07-27 01:44:37 -0500

    Time stamps, log file @ error.log, removed second parameter

--- sage-blade/Blade-BringUp/blade-bringup.sh
+++ sage-blade/Blade-BringUp/blade-bringup.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-echo "Connecting to iDRAC at $1"
+echo -e "Connecting to iDRAC at $1" | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'`
 
-if [[ $2 == "y" ]]
-then
-	echo "Machine Previously In Use"
-	echo "Erasing all previous data and settings"
-	python3 SystemEraseREDFISH.py -ip $1 -u root -p waggle -c AllApps,BIOS,CryptographicErasePD,DIAG,DrvPack,IDRAC,LCData,OverwritePD,PERCNVCache > output.txt
-else
-        echo "Fresh iDRAC Install"
-	echo "Erasing All Possible Stored Data"
-	python3 SystemEraseREDFISH.py -ip $1 -u root -p calvin -c AllApps,BIOS,CryptographicErasePD,DIAG,DrvPack,IDRAC,LCData,OverwritePD,PERCNVCache > output.txt
-fi
+echo "Erasing all previous data and settings" | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'`
+python3 SystemEraseREDFISH.py -ip $1 -u root -p calvin -c AllApps,BIOS,CryptographicErasePD,DIAG,DrvPack,IDRAC,LCData,OverwritePD,PERCNVCache | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'` > error.log
+python3 SystemEraseREDFISH.py -ip $1 -u root -p waggle -c AllApps,BIOS,CryptographicErasePD,DIAG,DrvPack,IDRAC,LCData,OverwritePD,PERCNVCache | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'` >> error.log
 
-echo "All Previous Data Erased, sleeping for 5 minutes while waiting for iDRAC to reboot"
+echo "All Previous Data Erased, sleeping for 5 minutes while waiting for iDRAC to reboot" | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'`
 #sleep 300
 secs=$((5 * 60))
 while [ $secs -gt 0 ]; do
@@ -22,11 +15,11 @@ while [ $secs -gt 0 ]; do
    : $((secs--))
 done
 
-echo "Setting iDRAC password to Waggle"
-python3 ChangeIdracUserPasswordREDFISH.py -ip $1 -u root -p calvin -id 2 -np waggle >> output.txt
+echo "Setting iDRAC password to Waggle" | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'` 
+python3 ChangeIdracUserPasswordREDFISH.py -ip $1 -u root -p calvin -id 2 -np waggle | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'` >> error.log
 
-echo "Turning Blade Back On, and sleeping for 3 minutes for Lifecycle reboot"
-python3 SetPowerStateREDFISH.py -ip $1 -u root -p waggle -r PushPowerButton >> output.txt
+echo "Turning Blade Back On, and sleeping for 3 minutes for Lifecycle reboot" | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'`
+python3 SetPowerStateREDFISH.py -ip $1 -u root -p waggle -r PushPowerButton | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'` >> error.log
 
 #sleep 180
 secs=$((3 * 60))
@@ -36,8 +29,8 @@ while [ $secs -gt 0 ]; do
    : $((secs--))
 done
 
-echo "Rebooting, and waiting 4 minutes to identify system"
-python3 SetPowerStateREDFISH.py -ip $1 -u root -p waggle -r PushPowerButton >> output.txt
+echo "Rebooting, and waiting 4 minutes to identify system" | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'`
+python3 SetPowerStateREDFISH.py -ip $1 -u root -p waggle -r PushPowerButton | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'` >> error.log
 
 #sleep 180
 secs=$((4 * 60))
@@ -47,20 +40,20 @@ while [ $secs -gt 0 ]; do
    : $((secs--))
 done
 
-echo "Break SSD RAID"
-python3 ConvertToNonRAIDREDFISH.py -ip $1 -u root -p waggle -n Disk.Bay.7:Enclosure.Internal.0-1:RAID.Integrated.1-1 >> output.txt
+echo "Break SSD RAID" | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'`
+python3 ConvertToNonRAIDREDFISH.py -ip $1 -u root -p waggle -n Disk.Bay.7:Enclosure.Internal.0-1:RAID.Integrated.1-1 | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'` >> error.log
 
-echo "Grabbing Machine ID, nowhere to send it for now"
+echo "Grabbing Machine ID, nowhere to send it for now" | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'`
 python3 ExportSystemConfigurationLocalREDFISH.py -ip $1 -u root -p waggle -t IDRAC > system.txt
 sed -n 16p system.txt
 
-echo "Ejecting any possible virtual media connected"
-python3 InsertEjectVirtualMediaREDFISH.py -ip $1 -u root -p waggle -o 2 -d 1 >> output.txt
+echo "Ejecting any possible virtual media connected" | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'`
+python3 InsertEjectVirtualMediaREDFISH.py -ip $1 -u root -p waggle -o 2 -d 1 | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'` >> error.log
 
-echo "Mounting Unattended ISO"
-python3 InsertEjectVirtualMediaREDFISH.py -ip $1 -u root -p waggle -o 1 -d 1 -i http://10.0.0.9/raj/autoinstall.iso >> output.txt
+echo "Mounting Unattended ISO" | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'`
+python3 InsertEjectVirtualMediaREDFISH.py -ip $1 -u root -p waggle -o 1 -d 1 -i http://10.0.0.9/raj/autoinstall.iso | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'` >> error.log
 
-echo "Setting boot to ISO and rebooting"
-python3 SetNextOneTimeBootVirtualMediaDeviceOemREDFISH.py -ip $1 -u root -p waggle -d 1 -r y >> output.txt
+echo "Setting boot to ISO and rebooting" | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'`
+python3 SetNextOneTimeBootVirtualMediaDeviceOemREDFISH.py -ip $1 -u root -p waggle -d 1 -r y | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'` >> error.log
 
-echo "Script Complete, please allow 20 minutes for OS to install and machine to be populated"
+echo "Script Complete, please allow 20 minutes for OS to install and machine to be populated" | xargs -L 1 echo `date +'[%Y-%m-%d %H:%M:%S]'`
