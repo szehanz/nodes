3744a061f289e075cd518207b5d91e5303b7a515
ozorob2 <omar.zorob@yahoo.com>
2020-07-30 16:23:37 -0500

    Delete mntdocker.sh

--- sage-blade/Blade-Image/mntdocker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-mkdir -p /media/plugin-data/
-rm -rf /var/lib/docker
-ln -s /media/plugin-data/ /var/lib/docker
-
-mount /dev/sda3 /media/plugin-data/
-
-logger "Docker mount: [$(mount | grep /dev/sda3)]"
