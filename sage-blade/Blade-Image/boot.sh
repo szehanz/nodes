661dc0dee0d43fa397ef7ad739094d41e5d7dac8
ozorob2 <omar.zorob@yahoo.com>
2020-07-30 16:38:25 -0500

    Update boot.sh

--- sage-blade/Blade-Image/boot.sh
+++ sage-blade/Blade-Image/boot.sh
@@ -35,10 +35,8 @@ echo "140.221.47.67 beehive" >> /etc/hosts
 ln -s /media/plugin-data /var/lib/docker
 apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common		
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -		
-add-apt-repository \		
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \		
-    $(lsb_release -cs) \		
-    stable"		
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get -y update 
 apt-get -y install docker-ce docker-ce-cli containerd.io		
 
 rm /etc/rc.local
